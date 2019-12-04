f1ce9b7151a9ff9a848e6ba6bff61b95cca49354
Phurba Sherpa <srpphurba@gmail.com>
2019-12-04 13:35:28 -0500

    change route for reviews

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/users', require('./reviews'))
+router.use('/reviews', require('./reviews'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
